343b31c573cf8e50692c82eb39357952e5f96f36
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-12-22 08:31:09 +0100

    🔧 Bug fix: element pouvait être null.

--- fonctions.js
+++ fonctions.js
@@ -1,6 +1,6 @@
 // Restriction des caractères dans les champs numériques
 function restrictionNombres(element, limite = 0) {
-    element.addEventListener("input", function (event) {
+    element?.addEventListener("input", function (event) {
         this.value = this.value.replace(/[^0-9]/g, "");
         if (limite != 0 && this.value.length > limite) {
             this.value = this.value.slice(0, limite);
@@ -10,7 +10,7 @@ function restrictionNombres(element, limite = 0) {
 
 // Restriction des caractères dans le champ alphabétique
 function restrictionAlphabet(element) {
-    element.addEventListener("input", function (event) {
+    element?.addEventListener("input", function (event) {
         this.value = this.value.replace(/[^a-zA-Z]/g, "");
     });
 }
